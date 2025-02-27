5a7222cfd17b37d9662f0cd1f35b68402020cbf0
Parmesaaan <tehdeadlifeyt@gmail.com>
2025-02-27 18:21:52 +0400

    Basic login

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
 <script setup lang="ts">
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import {ref} from "vue"
 
+const username = ref('')
+const password = ref('')
 </script>
 
 <template>
-  <router-view />
+  <div class="container">
+
+    <div>
+      <h2>Welcome</h2>
+    </div>
+
+    <div class="input-box">
+      <InputText v-model="username" />
+    </div>
+
+    <div class="input-box">
+      <InputText v-model="password" toggleMask />
+    </div>
+
+    <div class="button-group">
+      <Button label="Register" class="p-button-secondary" />
+      <Button label="Login" class="p-button-primary" />
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
+.container {
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+}
 
+.input-box {
+  width: 100%;
+  max-width: 300px;
+  padding: 8px;
+}
+
+.button-group {
+  padding: 8px;
+  display: flex;
+  gap: 12px;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,9 @@
-import { createApp } from 'vue'
 import './style.css'
+import 'primeflex/primeflex.css'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from "./router"
-import {createPinia} from "pinia"
 import PrimeVue from 'primevue/config'
-import Material from '@primeuix/themes/material'
 
 const app = createApp(App)
-app.use(createPinia())
-app.use(PrimeVue, {
-    theme: {
-        preset: Material,
-        options: {
-            prefix: 'p'
-        }
-    }
-})
-app.use(router)
+app.use(PrimeVue, { theme: 'none' })
 app.mount('#app')
