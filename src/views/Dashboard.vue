2559df4f9feaf7eb02d50f7e397c110c9c735c6c
Parmesaaan <tehdeadlifeyt@gmail.com>
2025-03-07 06:58:51 +0400

    Fix dashboard navigation

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import PanelMenu from 'primevue/panelmenu';
 import Divider from "primevue/divider";
-import Player from "../composables/Player.vue";
+import Player from "../composables/player/Player.vue";
 import {useStore} from "../stores";
 import {type Component, computed, ref} from "vue";
-import Group from "../composables/Group.vue";
+import Group from "../composables/group/Group.vue";
 import type {MenuItem} from "primevue/menuitem";
+import PlayerNotes from "../composables/player/PlayerNotes.vue";
+import Tasks from "../composables/player/Tasks.vue";
+import GroupJoin from "../composables/group/GroupJoin.vue";
+import GroupCreate from "../composables/group/GroupCreate.vue";
+import router from "../router";
 
 const store = useStore()
 const profile = store.profile!
@@ -14,64 +19,66 @@ const menuItems = ref<MenuItem[]>([
   {
     label: profile.user.username,
     icon: "pi pi-user",
-    key: "player",
     items: [
       {
-        label: "Notes",
-        icon: "pi pi-file",
-        key: "player-notes",
+        label: "My Account",
+        icon: "pi pi-id-card",
+        command: () => selectedComponent.value = Player
       },
       {
         label: "Tasks",
         icon: "pi pi-list",
-        key: "player-tasks",
+        command: () => selectedComponent.value = Tasks
+      },
+      {
+        label: "Notes",
+        icon: "pi pi-file",
+        command: () => selectedComponent.value = PlayerNotes
       },
     ]
   },
   {
     label: "Groups",
     icon: "pi pi-users",
-    key: "groups",
     items: [
       {
         label: "My Groups",
         icon: "pi pi-users",
-        key: "groups-my",
         items: profile.groups.map(group => ({
           label: group.name,
           icon: "pi pi-users",
-          key: `group-select-${group.id}`,
+          command: () => selectedComponent.value = Group
         })),
       },
       {
         label: "Join Group",
         icon: "pi pi-user-plus",
-        key: "group-join",
+        command: () => selectedComponent.value = GroupJoin
       },
       {
         label: "New Group",
         icon: "pi pi-plus-circle",
-        key: "group-new",
+        command: () => selectedComponent.value = GroupCreate
       },
     ]
   },
+  {
+    label: "Settings",
+    icon: "pi pi-cog",
+    disabled: true
+  },
+  {
+    label: "Logout",
+    icon: "pi pi-sign-out",
+    key: "logout",
+    command: () => {
+      store.logout()
+      router.push("/login")
+    }
+  },
 ])
 
-const selectedKey = ref<string | undefined>()
-
-const getSelectedComponent = (key?: string) => {
-  if (!key) return null;
-  if (key.startsWith("group")) return Group;
-  if (key.startsWith("player")) return Player;
-  return null;
-}
-
-let previousComponent: any = Player;
-const selectedComponent = computed(() => {
-  const result = getSelectedComponent(selectedKey.value)
-  if (result) previousComponent = result
-  return result || previousComponent
-})
+const selectedComponent = ref<Component>()
 
 </script>
 
@@ -82,7 +89,7 @@ const selectedComponent = computed(() => {
       <h2 class="font-medium text-gray-400 mt-1 mb-3">by <a href="https://github.com/Parmesaaan" target="_blank" rel="noopener noreferrer">Parmesaaan</a></h2>
       <Divider layout="horizontal"/>
       <PanelMenu :model="menuItems" class="w-64"/>
-      <p class="mt-auto">Test</p>
+      <p class="font-medium text-gray-400 text-xs mt-auto">version 0.0.1-beta</p>
     </div>
     <Divider layout="vertical" />
     <div>
